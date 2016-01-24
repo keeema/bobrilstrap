edd139ec3bb6b2fc60876125b1c983f848836909
keeema <rut.tomas@hotmail.com>
2016-01-24 16:57:21 +0100

    Added sr-only for labels

--- components/label/index.ts
+++ components/label/index.ts
@@ -3,16 +3,23 @@ import elem, { IBaseData } from 'bobrilstrap-element';
 
 export interface IData extends IBaseData {
     for?: string;
+    srOnly?: boolean;
 }
 
 interface ICtx extends b.IBobrilCtx {
     data: IData;
 }
 
+export let labelStyles = {
+    srOnly: b.styleDef('sr-only')
+};
+
 export default b.createDerivedComponent<IData>(elem, {
     id: 'bobrilstrap-label',
     render(ctx: ICtx, me: b.IBobrilNode) {
         me.tag = 'label';
+
+        b.style(me, !!ctx.data.srOnly && labelStyles.srOnly);
 
         if (ctx.data.for)
             me.attrs['for'] = ctx.data.for;

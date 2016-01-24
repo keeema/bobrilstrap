decb7619a3de9d079d11d49d810e75052ad91326
keeema <rut.tomas@hotmail.com>
2016-01-24 16:35:15 +0100

    Added input text

---
+++ components/input-text/example.ts
@@ -0,0 +1,23 @@
+import * as b from 'bobril';
+import core from 'bobrilstrap-core';
+import elem from 'bobrilstrap-element';
+import row from 'bobrilstrap-row';
+import container from 'bobrilstrap-container';
+import inputText, { Type } from './index';
+import col, { colStyles, Size } from 'bobrilstrap-col';
+
+let value = '';
+let onChange = (newVal: string) => { value = newVal; b.invalidate(); };
+
+b.init(() => {
+    console.log(value);
+    return [
+        core({}),
+        container({}, [
+            row({}, col({ size: Size.md, count: 6 }, [
+                inputText({ value: value.toString(), id: 'my-text1', onChange })
+            ])),
+            row({}, elem({}, value.toString())),
+        ])
+    ];
+});
---
+++ components/input-text/index.ts
@@ -0,0 +1,55 @@
+import * as b from 'bobril';
+import elem, { IBaseData } from 'bobrilstrap-element';
+
+export class Type {
+    static color: string = 'color';
+    static date: string = 'date';
+    static datetime: string = 'datetime';
+    static datetimeLocal: string = 'datetime-local';
+    static email: string = 'email';
+    static month: string = 'month';
+    static number: string = 'number';
+    static password: string = 'password';
+    static range: string = 'range';
+    static search: string = 'search';
+    static tel: string = 'tel';
+    static text: string = 'text';
+    static time: string = 'time';
+    static url: string = 'url';
+    static week: string = 'week';
+}
+
+export interface IData extends IBaseData {
+    value: string;
+    type?: Type;
+    disabled?: boolean;
+    readonly?: boolean;
+    onChange?: (value: string) => void;
+}
+
+
+interface ICtx extends b.IBobrilCtx {
+    data: IData;
+}
+
+export let inputTextStyles = {
+    formControl: b.styleDef('form-control')
+}
+
+export let create = b.createDerivedComponent<IData>(elem, {
+    id: 'bobrilstrap-input-text',
+    render(ctx: ICtx, me: b.IBobrilNode) {
+        me.tag = 'input';
+        me.attrs['type'] = ctx.data.type || 'text';
+        me.attrs.value = ctx.data.value;
+        b.style(me, inputTextStyles.formControl);
+
+        if (ctx.data.disabled)
+            me.attrs['disabled'] = 'disabled';
+
+        if (ctx.data.readonly)
+            me.attrs['readonly'] = 'readonly';
+    }
+});
+
+export default create;

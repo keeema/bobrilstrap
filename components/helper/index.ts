39ec3ce9ea533d0c235083feaca84559d4652d8f
keeema <rut.tomas@hotmail.com>
2016-01-30 10:56:32 +0100

    Added helper classes

---
+++ components/helper/index.ts
@@ -0,0 +1,31 @@
+import * as b from 'bobril';
+
+export default {
+    text: {
+        mutated: b.styleDef('text-mutated'),
+        primary: b.styleDef('text-primary'),
+        success: b.styleDef('text-success'),
+        info: b.styleDef('text-info'),
+        warning: b.styleDef('text-warning'),
+        danger: b.styleDef('text-danger')
+    },
+    background: {
+        primary: b.styleDef('bg-primary'),
+        success: b.styleDef('bg-success'),
+        info: b.styleDef('bg-info'),
+        warning: b.styleDef('bg-warning'),
+        danger: b.styleDef('bg-danger')
+    },
+    float: {
+        pullLeft: b.styleDef('pull-left'),
+        pullRight: b.styleDef('pull-right')
+    },
+    close: b.styleDef('close'),
+    caret: b.styleDef('caret'),
+    centerBlock: b.styleDef('center-block'),
+    show: b.styleDef('show'),
+    hidden: b.styleDef('hidden'),
+    srOnly: b.styleDef('sr-only'),
+    srOnlyFocusable: b.styleDef('sr-only-focusable'),
+    textHide: b.styleDef('text-hide')
+}

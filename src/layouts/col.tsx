9c80bf4a9a274d42e09697c60e99539aa10cf12d
Tomáš Růt <trut.cz@gmail.com>
2020-05-31 12:11:17 +0200

    Partial cols

--- src/layouts/col.tsx
+++ src/layouts/col.tsx
@@ -5,7 +5,22 @@ const styles = {
     col: b.styleDef("col"),
 };
 
-export interface IColData extends IBaseElementData {}
+export type IColSize = boolean | "auto" | number;
+
+//TODO: vygenerování stylů
+
+export interface IColData extends IBaseElementData {
+    span?: number;
+    xs?: IColSize;
+    sm?: IColSize;
+    md?: IColSize;
+    lg?: IColSize;
+
+    offsetXs?: number;
+    offsetSm?: number;
+    offsetMd?: number;
+    offsetLg?: number;
+}
 
 export class Col extends BaseElement<IColData> {
     static id: string = "bobrilstrap-col";

8fa6ca5d1719cbb0bf69fe705ccd8fc77308f6ff
Tomas Rut <t.rut@quadient.com>
2020-08-18 20:58:31 +0200

    Fix build

--- example/routes.tsx
+++ example/routes.tsx
@@ -2,6 +2,8 @@ import * as b from "bobril";
 import { Main } from "./main";
 import { Home } from "./home/home";
 import { IRouteWithName } from "./common/routing";
-export { documentation } from "./documentation/routeDefs";
+import { documentation as documentationDef } from "./documentation/routeDefs";
+
+export const documentation = documentationDef;
 export const main: b.IRoute = { handler: (data) => <Main {...data} /> };
 export const home: IRouteWithName = { url: "home", name: "home", handler: () => <Home /> };

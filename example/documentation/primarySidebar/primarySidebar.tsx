da300fe208c22b50135ce2d33f49d4df50d2f840
Tomas Rut <t.rut@quadient.com>
2020-08-18 23:43:09 +0200

    Make sidebar toggling reactive

--- example/documentation/primarySidebar/primarySidebar.tsx
+++ example/documentation/primarySidebar/primarySidebar.tsx
@@ -1,24 +1,34 @@
 import * as b from "bobril";
-import { Col, Collapse, Button } from "../../../index";
+import { Col, Button, display } from "../../../index";
 import { SidebarItem } from "./primarySidebarItem";
 import { documentation } from "../routeDefs";
-import { margin } from "../../../src/utilities/spacing";
-import { display } from "../../../src/utilities/display";
 
 export function PrimarySidebar(): b.IBobrilNode {
-    const smallDevice = window.innerWidth < 768;
-    // TODO: let it react to changed screen size
-    const [collapsed, setCollapsed] = b.useState(smallDevice);
+    const isSmallDevice = (): boolean => window.innerWidth < 768;
+    const smallDevice = b.useState(isSmallDevice());
+    const [collapsed, setCollapsed] = b.useState(smallDevice());
+
+    b.useLayoutEffect(() => {
+        const handler = (): void => {
+            if (smallDevice() !== isSmallDevice()) {
+                smallDevice(isSmallDevice());
+                setCollapsed(isSmallDevice());
+            }
+        };
+        window.addEventListener("resize", handler);
+        return () => window.removeEventListener("resize", handler);
+    }, []);
+
     return (
-        <Col md={3} xl={2} style={[sidebarStyle, smallDevice ? smallDeviceHeight : standardHeight]}>
-            <Button variant="dark" onClick={() => setCollapsed(!collapsed)} style={(margin("auto"), display("none", "md"))}>
+        <Col span={12} md={3} xl={2} style={[sidebarStyle, smallDevice() ? smallDeviceHeight : standardHeight]}>
+            <Button variant="dark" onClick={() => setCollapsed(!collapsed)} style={display("none", "md")}>
                 Part
             </Button>
-            <Collapse as="nav" style={linksStyle} collapsed={collapsed} onClick={() => setCollapsed(smallDevice)}>
+            <nav style={[linksStyle, collapsed && display("none")]} onClick={() => setCollapsed(smallDevice())}>
                 {documentation.subs.map((sub) => (
                     <SidebarItem route={sub} />
                 ))}
-            </Collapse>
+            </nav>
         </Col>
     );
 }

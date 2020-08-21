4e045d0cd3bddac4da07f708f01e73263aa102b3
Tomas Rut <t.rut@quadient.com>
2020-08-21 20:32:46 +0200

    Add prismjs

--- example/documentation/content/gettingStarted/parts/introduction.tsx
+++ example/documentation/content/gettingStarted/parts/introduction.tsx
@@ -45,6 +45,31 @@ export function Introduction(): b.IBobrilNode {
             <Anchor name="quick-start">
                 <h2>Quick Start</h2>
             </Anchor>
+            <pre>
+                <code class="language-tsx">{`import * as b from "bobril";
+interface IHelloData {
+  name: string;
+  children: b.IBobrilChildren;
+}
+class Hello extends b.Component<IHelloData> {
+  render(): b.IBobrilChildren {
+    return (
+      <>
+        <h1>Hello {this.data.name}</h1>
+        {this.data.children}
+      </>
+    );
+  }
+}
+b.init(() => (
+  <Hello name="Developer">
+    <p>
+      This is your first <strong>bobril</strong> application.
+    </p>
+  </Hello>
+));
+`}</code>
+            </pre>
             <LoremIpsum />
             <LoremIpsum />
             <Anchor name="test-1">
--- example/main.tsx
+++ example/main.tsx
@@ -1,7 +1,10 @@
 import * as b from "bobril";
+import * as prismJs from "prismjs";
 import { Navigation } from "./navigation/navigation";
 
 export function Main(data: b.IRouteHandlerData): b.IBobrilNode {
+    b.useEffect(() => prismJs.highlightAll(), []);
+
     return (
         <>
             <Navigation />

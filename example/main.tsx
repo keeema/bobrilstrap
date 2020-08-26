1b22685dc0deb14922cdd91816cabbc4b428cfe2
Tomas Rut <t.rut@quadient.com>
2020-08-26 21:44:39 +0200

    Update bobril and add styling for prismjs copy

--- example/main.tsx
+++ example/main.tsx
@@ -17,12 +17,16 @@ export function Main(data: b.IRouteHandlerData): b.IBobrilNode {
     );
 }
 
+const btnStyle = b.style({}, buttonStyles.btn).className!;
+const btnDarkStyle = b.style({}, buttonStyles["outline-dark"]).className!;
+const btnSmallStyle = b.style({}, buttonStyles.sizes("sm")).className!;
+
 function fixCopies(): void {
     const buttons = document.querySelectorAll("div.toolbar-item  button");
     for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i] as HTMLButtonElement;
         if (button.classList.length === 0) {
-            button.classList.add(buttonStyles.btn, buttonStyles["outline-dark"], buttonStyles.sizes("sm"));
+            button.classList.add(btnStyle, btnDarkStyle, btnSmallStyle);
         }
     }
 }
